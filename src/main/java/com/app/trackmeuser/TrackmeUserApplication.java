bff3e57684c586644814580167980e9121a9f0e4
jinhyeon <vmflwu123@gmail.com>
2023-09-27 23:03:00 +0900

    initial commit

---
+++ src/main/java/com/app/trackmeuser/TrackmeUserApplication.java
@@ -0,0 +1,13 @@
+package com.app.trackmeuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackmeUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrackmeUserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/trackmeuser/TrackmeUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.trackmeuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackmeUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
